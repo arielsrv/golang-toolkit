5bfc937161dd6de9bb37bb420dbe9479b6710e9e
ariel.pineiro <ariel.pineiro@iskaypet.com>
2022-11-04 12:46:26 +0100

    end

--- task/taskbuilder_test.go
+++ task/taskbuilder_test.go
@@ -4,9 +4,15 @@ import (
 	"github.com/arielsrv/golang-toolkit/task"
 	"github.com/stretchr/testify/assert"
 	"log"
+	"math/rand"
 	"testing"
 	"time"
 )
+
+func GetNumber() (int, error) {
+	time.Sleep(time.Millisecond * 1000)
+	return rand.Int(), nil
+}
 
 func TestBuilder_ForkJoin(t *testing.T) {
 	var future1, future2 *task.Task[int]
@@ -15,25 +21,17 @@ func TestBuilder_ForkJoin(t *testing.T) {
 
 	start := time.Now()
 	tb.ForkJoin(func(c *task.Awaitable) {
-		future1 = task.Await[int](c, func() (int, error) {
-			time.Sleep(time.Millisecond * 1000)
-			return 2, nil
-		})
-		future2 = task.Await[int](c, func() (int, error) {
-			time.Sleep(time.Millisecond * 1000)
-			return 3, nil
-		})
+		future1 = task.Await[int](c, GetNumber)
+		future2 = task.Await[int](c, GetNumber)
 	})
 
 	assert.NotNil(t, future1.Result)
 	assert.NoError(t, future1.Err)
-	assert.Equal(t, 2, future1.Result)
+	log.Println(future1.Result)
 
 	assert.NotNil(t, future2.Result)
 	assert.NoError(t, future2.Err)
-	assert.Equal(t, 3, future2.Result)
-
-	assert.Equal(t, 5, future1.Result+future2.Result)
+	log.Println(future2.Result)
 
 	end := time.Since(start)
 	log.Println(end)
